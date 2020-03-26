b68b4c70f5f8930713b4ef351d2a9b20764f5ef2
eocampo2728 <eduardo.ocampo2728@hotmail.com>
2020-03-26 09:33:35 -0500

    fourth

---
+++ src/main/java/edu/eci/arsw/model/Country.java
@@ -0,0 +1,57 @@
+package edu.eci.arsw.model;
+
+import org.json.JSONObject;
+
+public class Country {
+    private String name;
+    private int confirmed;
+    private int death;
+    private int recovered;
+    private JSONObject json;
+    
+    public Country (String name, int confirmed, int death, int recovered){
+        this.name = name;
+        this.confirmed = confirmed;
+        this.death = death;
+        this.recovered = recovered;
+    }
+    
+    public Country (JSONObject json){
+        this.json = json;
+        confirmed = Integer.valueOf(json.get("confirmed").toString()); 
+        death = Integer.valueOf(json.get("deaths").toString()); 
+        recovered = Integer.valueOf(json.get("recovered").toString()); 
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getConfirmed(){
+        return confirmed;
+    }
+    
+    public void setConfirmed(int confirmed){
+        this.confirmed = confirmed;
+    }
+    
+    public int getDeath(){
+        return death;
+    }
+    
+    public void setDeath(int death){
+        this.death = death;
+    }
+    
+    public int getRecovered(){
+        return recovered;
+    }
+    
+    public void setRecovered(int recovered){
+        this.recovered = recovered;
+    }
+}
