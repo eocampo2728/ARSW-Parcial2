cda3fbd8bbcf0d64b7079870e4bd65d4950637fe
eocampo2728 <eduardo.ocampo2728@hotmail.com>
2020-03-26 08:03:30 -0500

    Structure and model

--- coronavirusAPITest/src/main/java/edu/eci/arsw/model/Country.java
+++ coronavirusAPITest/src/main/java/edu/eci/arsw/model/Country.java
@@ -1,4 +1,47 @@
 package edu.eci.arsw.model;
 
 public class Country {
+    private String name;
+    private int confirmed;
+    private int death;
+    private int recovered;
+    
+    public Country (String name, int confirmed, int death, int recovered){
+        this.name = name;
+        this.confirmed = confirmed;
+        this.death = death;
+        this.recovered = recovered;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public int getConfirmed(){
+        return confirmed;
+    }
+    
+    public void setConfirmed(int confirmed){
+        this.confirmed = confirmed;
+    }
+    
+    public int getDeath(){
+        return death;
+    }
+    
+    public void setDeath(int death){
+        this.death = death;
+    }
+    
+    public int getRecovered(){
+        return recovered;
+    }
+    
+    public void setRecovered(int recovered){
+        this.recovered = recovered;
+    }
 }
